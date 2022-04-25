5c3ff27d3c1478b4832ee9a7d4306f5da44298e6
haddad <emha.haddad@gmail.com>
2022-04-26 05:34:55 +0700

    fix user repository id data type

--- src/main/java/id/ac/ui/cs/workout_tracker/repository/UserRepository.java
+++ src/main/java/id/ac/ui/cs/workout_tracker/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import id.ac.ui.cs.workout_tracker.model.User;
 
 
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, Integer>{
     User findByUserId(String string);
 }
